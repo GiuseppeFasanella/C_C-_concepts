a346daa00a5ddd7ab7b5683c986c818c78bbd27e
Giuseppe <I am Amerigo, you know my mail>
2015-05-20 15:07:44 +0200

    file handling

--- file_handling.C
+++ file_handling.C
@@ -27,11 +27,17 @@ plotter_smearing(){
 
 }
 
+//Scrivere su file
+#include <fstream> 
+std::ofstream ofs ("test.txt", std::ofstream::out);
+ofs << "lorem ipsum";
+ofs.close();
 
-/* If you want to write:                                                                                                                                                        
-  ofstream outfile;                                                                                                                                                             
-  outfile.open("tmp/Smearing.txt",ios::trunc);                                                                                                                                  
-  outfile <<event.smearings_ele1[i] <<endl;                                                                                                                                     
-  outfile.close();                                                                                                                                                              
-*/
+//in inputFile open for reading: the internal stream buffer supports input operations.
+//out* outputFile open for writing: the internal stream buffer supports output operations.
+//binary binaryOperations are performed in binary mode rather than text.
+//ate   at endThe output position starts at the end of the file.
+//app   appendAll output operations happen at the end of the file, appending to its existing contents.
+//trunc truncateAny contents that existed in the file before it is open are discarded.
+
 
