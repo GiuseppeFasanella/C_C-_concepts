0dba0aacb72c27db98d66f700b676b99e842eaa3
Giuseppe <I am Amerigo, you know my mail>
2015-05-25 12:22:19 +0200

    moving things around

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-
-int main() {
-  std::cout<<"ciao pippo!"<<'\n';
-  return 0;
-}
-
-//compile it with g++ -o prova2 pippo.cpp
-//g++ actually calls gcc with some options to let it know that it's a C++ code
