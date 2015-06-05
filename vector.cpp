eb9ffc562a70881bc517a28d9e77a1e37467e54f
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-06-05 17:58:07 +0200

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -8,13 +8,15 @@ main(){
   //Main advatage: no need to specify the length of a vector!
   vector<double> v;
   for(int i=0;i<4;i++){
-    v.push_back(9);
+    v.push_back(9); 
+    //If you don't declare the length of the vector you need to use push_back to fill the empty vector.
+    //After after filled, you can access v[0]. If you do it and it's still empty -> "seg fault"
   }
 
   cout<<v.size()<<endl;
 
   //On the other hand, if you want to specify the length, do it.
-  vector<double> v2(4,0.);
+  vector<double> v2(4,0.); //Here, you declared the length of the vector, so you can directly use v2[0]=5;
 
   //Navigate a vector. Either with indexes or using iterators
 
