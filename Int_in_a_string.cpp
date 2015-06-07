2feca9b213b9cf53bbb36255d0cb8b1f5e65bd88
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-06-07 19:46:47 +0200

    Numeric to string and viceversa

--- Int_in_a_string.cpp
+++ Int_in_a_string.cpp
@@ -1,2 +1,24 @@
+#### String to numeric
+
+float              stof(const string& str, size_t *idx = 0);
+double             stod(const string& str, size_t *idx = 0);
+long double        stold(const string& str, size_t *idx = 0);
+int                stoi(const string& str, size_t *idx = 0, int base = 10);
+long               stol(const string& str, size_t *idx = 0, int base = 10);
+unsigned long      stoul(const string& str, size_t *idx = 0, int base = 10);
+long long          stoll(const string& str, size_t *idx = 0, int base = 10);
+unsigned long long stoull(const string& str, size_t *idx = 0, int base = 10);
+
+# Example 
+std::string si = "98765";
+std::string sf = "3.14";
+int i = std::stoi(si);
+float f = std::stof(sf);
+
+#### Numeric to string 
+std::string s = std::to_string(42);
+
+## Puoi fare anche cosi', ma mi piace molto meno
 TString varName="";
 varName.Form("%d",var);
+
